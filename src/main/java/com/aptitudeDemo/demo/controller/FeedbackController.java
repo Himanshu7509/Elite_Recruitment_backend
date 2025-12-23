0ab0753e3e54e8113b6824296f82e741ed6493f1
amrutpaunikar <amrut0503003@gmail.com>
2025-12-23 11:01:03 +0530

    Adding feedback

---
+++ src/main/java/com/aptitudeDemo/demo/controller/FeedbackController.java
@@ -0,0 +1,51 @@
+package com.aptitudeDemo.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aptitudeDemo.demo.dto.student.FeedbackReq;
+import com.aptitudeDemo.demo.model.student.Feedback;
+import com.aptitudeDemo.demo.service.FeedbackService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/feedback")
+public class FeedbackController {
+
+    @Autowired
+    private FeedbackService feedbackService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Feedback>> getAllFeedbacks() {
+        log.info("Received request to fetch all students");
+        List<Feedback> feedbacks = feedbackService.getAllFeedbacks();
+        return ResponseEntity.ok(feedbacks);
+    }
+
+    @PostMapping("/submit")
+    public ResponseEntity<?> submitFeedback(@RequestBody FeedbackReq feedbackReq) {
+        
+        try {
+            Feedback savedFeedback= feedbackService.saveFeedback(feedbackReq);
+            log.info("Successfully saved student form with ID: {}", savedFeedback.getId());
+            
+           
+            return ResponseEntity.ok(savedFeedback);
+        } catch (Exception e) {
+            log.error("Error saving student form: ", e);
+            return ResponseEntity.status(500).body("Error saving student form: " + e.getMessage());
+        }
+    }
+
+}
