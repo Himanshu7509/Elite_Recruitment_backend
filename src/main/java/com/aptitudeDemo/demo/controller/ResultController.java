2298852669060c7a43004c7b805a0f3e5a874eee
Aryan Nagardhankar <aryannagardhankar2020@gmail.com>
2025-12-24 12:33:55 +0530

    add get all and getById

--- src/main/java/com/aptitudeDemo/demo/controller/ResultController.java
+++ src/main/java/com/aptitudeDemo/demo/controller/ResultController.java
@@ -1,16 +1,19 @@
 package com.aptitudeDemo.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.aptitudeDemo.demo.dto.student.FeedbackReq;
 import com.aptitudeDemo.demo.dto.student.ResultReq;
-import com.aptitudeDemo.demo.model.student.Feedback;
 import com.aptitudeDemo.demo.model.student.Result;
 import com.aptitudeDemo.demo.service.ResultService;
 
@@ -25,6 +28,24 @@ public class ResultController {
     @Autowired
     private ResultService resultService;
 
+    @GetMapping("/all")
+    public ResponseEntity<List<Result>> getAllResults() {
+        log.info("Received request to fetch all results");
+        List<Result> results = resultService.getAllResults();
+        return ResponseEntity.ok(results);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getResultById(@PathVariable String id) {
+        log.info("Received request to fetch result by ID: {}", id);
+        Optional<Result> result = resultService.getResultById(id);
+        if (result.isPresent()) {
+            return ResponseEntity.ok(result.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
     @PostMapping("/submit")
     public ResponseEntity<?> submitResult(@RequestBody ResultReq resultReq) {
         
