d084d24eb47eb200cd6e58a529f3da84918e34a2
Aryan Nagardhankar <aryannagardhankar2020@gmail.com>
2025-12-26 14:29:33 +0530

    Done

--- src/main/java/com/aptitudeDemo/demo/dto/student/MobileVerificationRequest.java
+++ src/main/java/com/aptitudeDemo/demo/dto/student/MobileVerificationRequest.java
@@ -1,8 +1,10 @@
 package com.aptitudeDemo.demo.dto.student;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
+@Data @AllArgsConstructor @NoArgsConstructor
 public class MobileVerificationRequest {
     private String mobileNumber;
 }
