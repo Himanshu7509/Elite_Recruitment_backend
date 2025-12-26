e56743dfa8287d5a63c5621bed19f3205ef0fa99
Aryan Nagardhankar <aryannagardhankar2020@gmail.com>
2025-12-26 13:30:34 +0530

    add contoller

--- src/main/java/com/aptitudeDemo/demo/controller/MobileVerificationController.java
+++ src/main/java/com/aptitudeDemo/demo/controller/MobileVerificationController.java
@@ -1,13 +1,19 @@
 package com.aptitudeDemo.demo.controller;
 
-import com.aptitudeDemo.demo.dto.student.MobileVerificationRequest;
-import com.aptitudeDemo.demo.service.MobileVerificationService;
-import lombok.extern.slf4j.Slf4j;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import com.aptitudeDemo.demo.dto.student.MobileVerificationRequest;
+import com.aptitudeDemo.demo.service.MobileVerificationService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/mobile-verification")
