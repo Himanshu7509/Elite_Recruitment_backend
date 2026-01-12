dc92a66585228c6e5968c5ddc1122e47256d5774
Aryan Nagardhankar <aryannagardhankar2020@gmail.com>
2026-01-12 06:03:42 +0530

    add

--- src/main/java/com/aptitudeDemo/demo/repository/QuestionsRepository.java
+++ src/main/java/com/aptitudeDemo/demo/repository/QuestionsRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.aptitudeDemo.demo.model.OpenAI.Questions;
 
+@Repository
 public interface QuestionsRepository extends MongoRepository<Questions, String> {
 
     List<Questions> findByEmail(String email);
--- src/main/java/com/aptitudeDemo/demo/repository/ResultRepository.java
+++ src/main/java/com/aptitudeDemo/demo/repository/ResultRepository.java
@@ -3,9 +3,11 @@ package com.aptitudeDemo.demo.repository;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.aptitudeDemo.demo.model.student.Result;
 
+@Repository
 public interface ResultRepository extends MongoRepository<Result,String> {
 
     Optional<Result> findByFullName(String fullName);
